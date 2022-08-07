bbb65a7582312c78304fcf53009fee4ee6c39ded
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-08-07 15:29:17 -0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 require('dotenv').config();
 const {PORT:port, POSTGRES_CONNECTION_STRING:pgstring} = process.env;
 
-const users = await fetch("https://fakerapi.it/api/v1/persons?_quantity=1000").then(resp => resp.json());
-console.log(users.data);
+async function GetFakeUsers ()
+{
+    const users = await fetch("https://fakerapi.it/api/v1/persons?_quantity=1000").then(resp => resp.json());
+    console.log(users.data);
+}
+GetFakeUsers ();
 
 app.listen(port, () => {console.log(`Server started at http://localhost:${port}`)});
