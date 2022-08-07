f75c3c1ffd4b7905810ac6db41e543c23849f953
Luis Gustavo Cardoso Zanetti Pereira <luisgustavoczp@gmail.com>
2022-08-07 18:50:58 +0000

    update teste

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+
 const app = express();
 require('dotenv').config();
 const {PORT:port, POSTGRES_CONNECTION_STRING:pgstring} = process.env;
 
-async function GetFakeUsers ()
+const resp = require("./dadosusers");
+const dados = JSON.parse(resp);
+
+function insertQuery (user)
 {
-    const users = await fetch("https://fakerapi.it/api/v1/persons?_quantity=1000").then(resp => resp.json());
-    console.log(users.data);
+    if(!user.firstname) return;
+    return `
+    <p>
+        INSERT INTO users(firstname, lastname, email, phone, birthday, gender) VALUES("${user.firstname}", "${user.lastname}", "${user.email}", "${user.phone}", "${user.birthday}", "${user.gender}");
+    </p>
+    `;
 }
-GetFakeUsers ();
+
+app.get ("/", (req, res) =>
+{
+    const r = `${dados.data.map(user => insertQuery(user))}`;
+    res.send(r);
+});
 
 app.listen(port, () => {console.log(`Server started at http://localhost:${port}`)});
