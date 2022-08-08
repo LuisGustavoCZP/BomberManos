3cfbe4c4b3628465f1a2a28aec4ada7d4fa54eae
Luis Gustavo Cardoso Zanetti Pereira <luisgustavoczp@gmail.com>
2022-08-08 01:17:19 +0000

    update teste

--- index.js
+++ index.js
@@ -1,30 +1,84 @@
 const express = require("express");
+const Redis = require("ioredis");
 const { Pool } = require('pg')
 const app = express();
 require('dotenv').config();
 const {PORT:port, POSTGRES_CONNECTION_STRING:pgstring} = process.env;
 
-/* const resp = require("./dadosusers");
-const dados = JSON.parse(resp);
-
-function insertQuery (user)
-{
-    if(!user.firstname) return;
-    return `
-    <p>
-        INSERT INTO public.users ("firstname", "lastname", "email", "phone", "birthday", "gender") VALUES ($$${user.firstname}$$, $$${user.lastname}$$, $$${user.email}$$, '${user.phone}', '${user.birthday}', '${user.gender}');
-    </p>
-    `;
-} */
-
 const db = new Pool({connectionString:pgstring});
+const redis = new Redis();
+
+async function cachingRedis ()
+{
+    await redis.ltrim("users", 0, 0);
+    const resp = await db.query('SELECT * FROM users');
+    for(const r of resp.rows)
+    {
+        const id = `user:${r.id}`;
+        await redis.lpush("users", JSON.stringify(r));
+        //await redis.hset(id, r);
+    };
+}
+cachingRedis ();
+
+let resReq = null;
+let mode = "";
+let initTime = 0;
+let total = 0;
+let count = 0;
+function TesteComputing()
+{
+    count++;
+    if(count == total)
+    {
+        const f = performance.now() - initTime;
+        const s = `Performance of ${mode}(${total/1000}K) = ${f}ms`;
+        mode = '';
+        total = 0;
+        count = 0;
+        initTime = 0;
+        //console.log(s);
+        resReq.send(s);
+    }
+}
+
+app.get ("/postgres", (req, res) =>
+{
+    resReq = res;
+    mode = "Postgres";
+    total = Number(req.query.tests);
+    initTime = performance.now();
+    for(let i = 0; i < total; i++)
+    {
+        db.query('SELECT * FROM users').then(TesteComputing);
+    }
+});
+
+app.get ("/redis", (req, res) =>
+{
+    resReq = res;
+    mode = "Redis";
+    total = Number(req.query.tests);
+    initTime = performance.now();
+    for(let i = 0; i < total; i++)
+    {
+        redis.lrange("users", 0, -1).then(TesteComputing);
+    }
+});
 
 app.get ("/", async (req, res) =>
 {
-    const resp = await db.query('SELECT * FROM users');
-    
-    const r = `${resp.rows.map(user => `<p>${JSON.stringify(user)}</p>`)}`;
-    res.send(r);
+    res.sendFile(__dirname+"/public/index.html");   
+});
+
+app.get ("/static/index.js", async (req, res) =>
+{
+    res.sendFile(__dirname+"/public/index.js");
+});
+
+app.get ("/static/style.css", async (req, res) =>
+{
+    res.sendFile(__dirname+"/public/style.css");
 });
 
 app.listen(port, () => {console.log(`Server started at http://localhost:${port}`)});
