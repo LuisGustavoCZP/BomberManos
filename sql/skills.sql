f588eb46b8f96b997ea16b62e269c618267954e7
Luis Gustavo Cardoso Zanetti Pereira <luisgustavoczp@gmail.com>
2022-08-13 02:19:13 +0000

    update sql

--- sql/skills.sql
+++ sql/skills.sql
@@ -1,6 +1,6 @@
 INSERT INTO skills (name, type, description, price, delay, cost) VALUES
-    ('Inteligente', 'permantent', 'Aumenta em 50% o ganho de experiência.', NULL, NULL, NULL),
-    ('Ligeirinho', 'permantent', 'Aumenta em 25% a velocidade de movimento.', NULL, NULL, NULL);
+    ('Inteligente', 1, 'Aumenta em 50% o ganho de experiência.', NULL, NULL, NULL),
+    ('Ligeirinho', 1, 'Aumenta em 25% a velocidade de movimento.', NULL, NULL, NULL);
 
 INSERT INTO skill_effects (skill_id, level, effect_id) VALUES
     (0, 1, 0),
